99cf24b064ab580316b7d28c5d0f6fa85c3ad842
sergio <sergiofdezfernandez@gmail.com>
2021-05-03 19:22:40 +0200

    Firebase Authentication with auth activity and home activity

---
+++ app/src/main/java/com/uniovi/justbeer/AuthActivity.kt
@@ -0,0 +1,67 @@
+package com.uniovi.justbeer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.uniovi.justbeer.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUp()
+    }
+
+    private fun setUp() {
+        title = "Autenticación"
+        binding.signUpButton.setOnClickListener {
+            if (binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.emailEditText.text.toString(),
+                    binding.passwordEditText.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+        binding.loginButton.setOnClickListener {
+            if (binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    binding.emailEditText.text.toString(),
+                    binding.passwordEditText.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent: Intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+}
