35769e6392ae73e2c2a7115898ea3a32ff873dc0
ManuelJunco <uo252010@uniovi.es>
2021-05-09 11:40:28 +0200

    maquetacion

--- app/src/main/java/com/uniovi/justbeer/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/uniovi/justbeer/ui/favorites/FavoritesFragment.kt
@@ -6,15 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.squareup.picasso.Picasso
-import com.uniovi.justbeer.R
 import com.uniovi.justbeer.databinding.FavoritesFragmentBinding
-import com.uniovi.justbeer.databinding.HomeFragmentBinding
-import com.uniovi.justbeer.ui.home.HomeViewModel
-import com.uniovi.justbeer.ui.home.adapters.BeerListAdapter
+
 
 class FavoritesFragment : Fragment() {
 
@@ -28,7 +22,6 @@ class FavoritesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FavoritesFragmentBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
@@ -38,6 +31,8 @@ class FavoritesFragment : Fragment() {
         viewModel.recommendation.observe(viewLifecycleOwner, {
             Picasso.get().load(it.image).resize(250, 550).into(binding.recommendationImageView)
             binding.recommendationTextView.text = it.name
+            binding.recommendationAbvTextView.text = "${it.alcohol}%"
+            binding.recommendationDescriptionTextView.text = it.description
         })
         viewModel.requestRecomendation()
     }
